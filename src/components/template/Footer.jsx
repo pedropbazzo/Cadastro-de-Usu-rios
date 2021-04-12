126d0176a9b82506438fe93cff00e59cbd53a930
pedropbazzo <pedropbazzo@outlook.com>
2021-04-12 05:32:41 -0300

    first and only commit =)

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido por
+            <strong><a href="https://github.com/pedropbazzo"> Pedro Bazzo </a><i class="fa fa-rocket"></i></strong> 
+        </span>
+    </footer>
